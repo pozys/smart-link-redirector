cf6f3876f9f6902b57228329de75462c234e0403
Культяпов Константин <pozys@mail.ru>
2025-03-10 20:21:07 +0300

    drop unused code

--- app/Application/Interfaces/RedirectLinkRepositoryInterface.php
+++ app/Application/Interfaces/RedirectLinkRepositoryInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Application\Interfaces;
 
-use App\Application\DTO\RedirectLinkDto;
 use App\Domain\Interfaces\LinkInterface;
 use App\Domain\Models\Rules\Rule;
 
@@ -12,6 +11,4 @@ interface RedirectLinkRepositoryInterface
 {
     /** @return Rule[] */
     public function findRedirects(LinkInterface $link): array;
-
-    public function save(RedirectLinkDto $redirect): void;
 }
